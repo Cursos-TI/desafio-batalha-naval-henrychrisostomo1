a54a9314ddacb78856a72e498c0ce26967af251b
henrychrisostomo1 <HENRYCHRISOSTOMO1@GMAIL.COM>
2025-10-09 19:07:34 +0000

    batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,21 +4,40 @@
 int main() {
     
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
     int tabuleiro [10][10];
-
-    printf(" TABULEIRO BATALHA NAVAL \n");
-    printf("  ");
     
-    for (int j = 0; j <= 10; j++)
-    {
-        printf("%c ",  linha[j]);
+    
+    for(int i = 0; i < 10; i++){
+      for (int j = 0; j < 10; j++){
+        tabuleiro[i][j] = 0;
+    
+      }
     }
 
-    for(int i = 0; i <= 10; i++)
-    {
-        printf ("%d \n", i);
-      
-      
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[2][1 + i] = 3;
+
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[5 + i][7] = 3;
+    }
+
+    printf("--- TABULEIRO BATALHA NAVAL ---\n\n");
+    printf("  ");
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", linha[j]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("2%d ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+        
     return 0;
 }
